8549ef3a549f0aaac2ef78473b4c01873fd47228
COR Students <corstudents@Matts-MBP-2.localdomain>
2018-10-22 12:14:35 -0400

    Assessment complete

---
+++ Assessment1.js
@@ -0,0 +1,54 @@
+
+//Question 1
+let place = "Atlanta";
+let population = 30;
+let win = true;
+let time = 2.5;
+
+//Question 2
+let trueOrFalse = true
+if(trueOrFalse== true){
+    return 1
+} else{
+    return 2
+}
+
+//Question 3
+for(i=0; i<= 100; i+=2){
+    console.log(i);
+}
+
+//Question 4
+let greeting = true;
+let counter = 0;
+while(greeting){
+    console.log("hello")
+    counter ++
+    if (counter== 20){
+        greeting = false;
+    }
+}
+
+
+//Question 5
+let single = status(false);
+let relationship = status(true);
+
+function status(value){
+    if(value===true){
+        return relationship
+    }else{
+        return single}
+    
+    if(relationship===true){
+    console.log("true")
+    }
+    if(single===false){
+    console.log("false")
+            }
+        }
+
+// Bonus
+D- dont
+R- repeat
+Y- yourself
